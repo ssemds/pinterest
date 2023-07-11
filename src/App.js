53fb4de106322ca7efca5a280e3a41f36f0dfc0c
kanybekovna04 <aidinakanybekovna@gmail.com>
2023-07-11 20:29:20 +0600

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import MainRoutes from "./components/Routes/MainRoutes";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
       <MainRoutes />
+      <Footer/>
+
     </div>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="footer"
+      style={{
+        bottom: "0",
+        backgroundColor: "bisque",
+        color: "#cbcbcb",
+        height: "50px",
+      }}
+    ></div>
+  );
 };
 
 export default Footer;
